ba7e630202028a40be1139dc9bec90ab195e1e09
z-felixhan <34609664+z-felixhan@users.noreply.github.com>
2019-08-25 00:48:49 -0400

    Added calls to functions merged() and dl5(). Added a check for elapsed time.

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import Components as c
+import time
+
+start_time = time.time()
 
 dowURL = "https://ca.finance.yahoo.com/quote/%5EDJI/components?p=%5EDJI"
 baseURL = "https://ca.finance.yahoo.com/quote/"
@@ -7,7 +10,11 @@ statsPath = "/key-statistics"
 stocks = c.stocks(dowURL)
 stocksCurrentPrice = c.currentPrice(baseURL, stocks)
 stocksDividend = c.dividend(baseURL, statsPath, stocks)
+merged = c.merge(stocks, stocksCurrentPrice, stocksDividend)
+dl5 = c.dl5(merged)
 
-print(stocks)
-print(stocksCurrentPrice)
-print(stocksDividend)
+print("The below is the DOW Low 5 in the format of [STOCK, CURRENT PRICE, TRAILING DIVIDEND]:")
+print(dl5)
+
+elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+print("Elapsed time: " + elapsed_time)
