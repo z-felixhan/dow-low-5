44764876d34d395e55fabe7712da1651a7e5e649
z-felixhan <34609664+z-felixhan@users.noreply.github.com>
2022-03-20 14:40:30 -0400

    Rename

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Components as c
+import components as c
 import time
 
 start_time = time.time()
@@ -13,7 +13,8 @@ stocksDividend = c.dividend(baseURL, statsPath, stocks)
 merged = c.merge(stocks, stocksCurrentPrice, stocksDividend)
 dl5 = c.dl5(merged)
 
-print("The below is the DOW Low 5 in the format of [STOCK, CURRENT PRICE, TRAILING DIVIDEND]:")
+print(
+    "The below is the DOW Low 5 in the format of [STOCK, CURRENT PRICE, TRAILING DIVIDEND]:")
 print(dl5)
 
 elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
